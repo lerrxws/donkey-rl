3b8875cb075d32ad2dd77cc2143b697f6eb3d9b2
Daniil Kucherenko <dania_kucherenko@outlook.com>
2026-05-02 16:01:00 +0200

    create replay buffer

---
+++ agents/replay_buffer.py
@@ -0,0 +1,66 @@
+from collections import deque
+import random
+
+
+class ReplayBuffer():
+    def __init__(self):
+        self.buffer =deque(maxlen=10000)
+    
+    def __len__(self):
+        return len(self.buffer)
+
+    def push(self,state,action,reward,next_state,done):
+        self.buffer.append((state,action,reward,next_state,done))
+
+    def sample(self,batch_size=32):
+        if len(self.buffer)<batch_size:
+            return None
+        return random.sample(self.buffer,batch_size)
+
+
+
+if __name__=="__main__":
+    replay_buffer=ReplayBuffer()
+    replay_buffer.push([0, 0, 80],0,0.1,[0, 0, 60],False)
+    replay_buffer.push([0, 0, 60],0,0.1,[0, 0, 20],False)
+    replay_buffer.push([0, 0, 20],0,0.1,[0, 0, 0],True)
+    replay_buffer.push([0, 0, 80],0,0.1,[0, 0, 60],False)
+    replay_buffer.push([0, 0, 60],0,0.1,[0, 0, 20],False)
+    replay_buffer.push([0, 0, 20],0,0.1,[0, 0, 0],True)
+    replay_buffer.push([0, 0, 80],0,0.1,[0, 0, 60],False)
+    replay_buffer.push([0, 0, 60],0,0.1,[0, 0, 20],False)
+    replay_buffer.push([0, 0, 20],0,0.1,[0, 0, 0],True)
+    replay_buffer.push([0, 0, 80],0,0.1,[0, 0, 60],False)
+    replay_buffer.push([0, 0, 60],0,0.1,[0, 0, 20],False)
+    replay_buffer.push([0, 0, 20],0,0.1,[0, 0, 0],True)
+    replay_buffer.push([0, 0, 80],0,0.1,[0, 0, 60],False)
+    replay_buffer.push([0, 0, 60],0,0.1,[0, 0, 20],False)
+    replay_buffer.push([0, 0, 20],0,0.1,[0, 0, 0],True)
+    replay_buffer.push([0, 0, 80],0,0.1,[0, 0, 60],False)
+    replay_buffer.push([0, 0, 60],0,0.1,[0, 0, 20],False)
+    replay_buffer.push([0, 0, 20],0,0.1,[0, 0, 0],True)
+    replay_buffer.push([0, 0, 80],0,0.1,[0, 0, 60],False)
+    replay_buffer.push([0, 0, 60],0,0.1,[0, 0, 20],False)
+    replay_buffer.push([0, 0, 20],0,0.1,[0, 0, 0],True)
+    replay_buffer.push([0, 0, 80],0,0.1,[0, 0, 60],False)
+    replay_buffer.push([0, 0, 60],0,0.1,[0, 0, 20],False)
+    replay_buffer.push([0, 0, 20],0,0.1,[0, 0, 0],True)
+    replay_buffer.push([0, 0, 80],0,0.1,[0, 0, 60],False)
+    replay_buffer.push([0, 0, 60],0,0.1,[0, 0, 20],False)
+    replay_buffer.push([0, 0, 20],0,0.1,[0, 0, 0],True)
+    replay_buffer.push([0, 0, 80],0,0.1,[0, 0, 60],False)
+    replay_buffer.push([0, 0, 60],0,0.1,[0, 0, 20],False)
+    replay_buffer.push([0, 0, 20],0,0.1,[0, 0, 0],True)
+    replay_buffer.push([0, 0, 80],0,0.1,[0, 0, 60],False)
+    replay_buffer.push([0, 0, 60],0,0.1,[0, 0, 20],False)
+    replay_buffer.push([0, 0, 20],0,0.1,[0, 0, 0],True)
+    replay_buffer.push([0, 0, 80],0,0.1,[0, 0, 60],False)
+    replay_buffer.push([0, 0, 60],0,0.1,[0, 0, 20],False)
+    replay_buffer.push([0, 0, 20],0,0.1,[0, 0, 0],True)
+    replay_buffer.push([0, 0, 80],0,0.1,[0, 0, 60],False)
+    replay_buffer.push([0, 0, 60],0,0.1,[0, 0, 20],False)
+    replay_buffer.push([0, 0, 20],0,0.1,[0, 0, 0],True)
+    replay_buffer.push([0, 0, 80],0,0.1,[0, 0, 60],False)
+    replay_buffer.push([0, 0, 60],0,0.1,[0, 0, 20],False)
+    replay_buffer.push([0, 0, 20],0,0.1,[0, 0, 0],True)
+    print(replay_buffer.sample())

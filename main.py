b02f08e0a5957a2f5f1c1841543d203669846fcf
Daniil Kucherenko <164510375+Oroch1maru@users.noreply.github.com>
2026-05-10 13:45:26 +0200

    Add finish step #58

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ if __name__ == "__main__":
     run_training(
         mode="double_dqn",
         num_episodes=20000, 
-        step_interval=0.2
+        step_interval=0.2,
+        max_episode_steps=500
     )
